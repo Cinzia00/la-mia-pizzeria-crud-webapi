bf2b6fce417179ab29d80336357292c48962c3c0
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-10-09 15:06:13 +0200

    creato controller api

---
+++ Controllers/API/PizzasController.cs
@@ -0,0 +1,24 @@
+﻿using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetPizze()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = db.Pizze.ToList();
+
+                return Ok(pizze);
+            }
+        }
+    }
+}
